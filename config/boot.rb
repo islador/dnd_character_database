e2af706db5f345ebdb00614688595f9f4d6fe8ee
Lucas Mercado <luke.isla@gmail>
2015-03-01 11:06:05 -0800

    Added execjs to the gemfile and forced it to use nodeJS.

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
+# http://stackoverflow.com/questions/9686868/how-do-i-force-execjs-to-use-node-js
+ENV['EXECJS_RUNTIME'] = 'Node'
+
 require 'bundler/setup' # Set up gems listed in the Gemfile.
